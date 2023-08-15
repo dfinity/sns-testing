cd45df2a476382acd29c9ce6f70c63a4f956d87b
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2023-08-15 11:42:48 +0200

    Fix typo

--- constants.sh
+++ constants.sh
@@ -175,5 +175,5 @@ if [[ -f sns_canister_ids.json ]]; then
   export SNS_INDEX_CANISTER_ID=$(jq -r '.index_canister_id' sns_canister_ids.json)
   export SNS_LEDGER_CANISTER_ID=$(jq -r '.ledger_canister_id' sns_canister_ids.json)
   export SNS_ROOT_CANISTER_ID=$(jq -r '.root_canister_id' sns_canister_ids.json)
-  export SNS_SWAP_CANISTER_ID=$(jq -r '.root_canister_id' sns_canister_ids.json)
+  export SNS_SWAP_CANISTER_ID=$(jq -r '.swap_canister_id' sns_canister_ids.json)
 fi
