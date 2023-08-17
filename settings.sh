28790d954636b0477694f6e47491f62d08e09acf
Arshavir Ter-Gabrielyan <arshavir.ter.gabrielyan@dfinity.org>
2023-08-17 20:54:22 +0200

    Bump IC_COMMIT

--- settings.sh
+++ settings.sh
@@ -26,6 +26,6 @@ export NNS_DAPP_RELEASE="nightly-2023-08-08"
 # $ ./gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh origin/master
 # from the IC monorepo: https://github.com/dfinity/ic
 # if you change IC_COMMIT, then you need to rerun `source install.sh`
-export IC_COMMIT="f8dfb620a62672cd863689f7a29f164aa3e656a7"
+export IC_COMMIT="f99495f3772d5a85d25ef5008179b49a5f12c5c2"
 
 export TESTNET="local"
