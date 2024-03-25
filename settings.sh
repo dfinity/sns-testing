2c085b3282da32fde2fe261d21ea107f88c11de7
Martin Raszyk <martin.raszyk@dfinity.org>
2024-03-25 15:34:49 +0100

    bump version

--- settings.sh
+++ settings.sh
@@ -15,17 +15,17 @@ export DX_IDENT="default"
 
 # you can find available II releases here:
 # https://github.com/dfinity/internet-identity/tags
-export II_RELEASE="release-2024-03-01"
+export II_RELEASE="release-2024-03-22"
 
 # you can find NNS proposals upgrading system canisters here:
 # https://dashboard.internetcomputer.org/governance?topic=TOPIC_NETWORK_CANISTER_MANAGEMENT
 # NNS proposals to upgrade NNS frontend dapp are called "Upgrade Nns Canister: qoctq-giaaa-aaaaa-aaaea-cai"
-export NNS_DAPP_RELEASE="proposal-127458"
+export NNS_DAPP_RELEASE="proposal-128768"
 
 # only edit IC_COMMIT to a commit to master with disk image obtained via:
 # $ ./gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh origin/master
 # from the IC monorepo: https://github.com/dfinity/ic
 # if you change IC_COMMIT, then you need to rerun `source install.sh`
-export IC_COMMIT="d1315a1b9b002f177526faa5ee6d7e1e1aa3a22a"
+export IC_COMMIT="58d47e86a0a30953f4eb1eb615b32a17e04400ff"
 
 export TESTNET="local"
