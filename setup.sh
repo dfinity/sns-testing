8e4e422f8e28269c11f880d4cda856ee843c473f
Martin Raszyk <martin.raszyk@dfinity.org>
2024-05-24 13:45:24 +0200

    comment

--- setup.sh
+++ setup.sh
@@ -78,7 +78,10 @@ then
   curl -L "https://github.com/dfinity/nns-dapp/releases/download/${SNS_AGGREGATOR_RELEASE}/sns_aggregator_dev.wasm.gz" -o nns-dapp/out/sns_aggregator.wasm
 fi
 
+# Install the SNS aggregator and configure it to refresh every second so that the NNS dapp is as up-to-date as possible.
+# This is possible in local testing as we can afford the additional load in local testing.
 ${DFX} canister install sns_aggregator --network "${NETWORK}" --wasm nns-dapp/out/sns_aggregator.wasm --argument '(opt record { update_interval_ms = 1000:nat64; fast_interval_ms = 100:nat64; })'
+
 ${DFX} canister install internet_identity --network "${NETWORK}" --wasm internet-identity/internet_identity_dev.wasm --argument '(null)'
 ${DFX} canister install nns-dapp --network "${NETWORK}" --wasm nns-dapp/out/nns-dapp.wasm --argument '(opt record{
   args = vec {
