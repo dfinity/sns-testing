eb0c25aa6978c088e14a86966fc2c86ff23cb074
Martin Raszyk <martin.raszyk@dfinity.org>
2024-05-24 10:07:01 +0200

    fix type

--- setup.sh
+++ setup.sh
@@ -78,7 +78,7 @@ then
   curl -L "https://github.com/dfinity/nns-dapp/releases/download/${SNS_AGGREGATOR_RELEASE}/sns_aggregator_dev.wasm.gz" -o nns-dapp/out/sns_aggregator.wasm
 fi
 
-${DFX} canister install sns_aggregator --network "${NETWORK}" --wasm nns-dapp/out/sns_aggregator.wasm '(opt record { update_interval_ms = 1000; fast_interval_ms = 100; })'
+${DFX} canister install sns_aggregator --network "${NETWORK}" --wasm nns-dapp/out/sns_aggregator.wasm '(opt record { update_interval_ms = 1000:nat64; fast_interval_ms = 100:nat64; })'
 ${DFX} canister install internet_identity --network "${NETWORK}" --wasm internet-identity/internet_identity_dev.wasm --argument '(null)'
 ${DFX} canister install nns-dapp --network "${NETWORK}" --wasm nns-dapp/out/nns-dapp.wasm --argument '(opt record{
   args = vec {
