642bb3fe1afa3310e3a37ff860c12f51157be4a1
Your Name <you@example.com>
2024-07-04 22:35:24 +0200

    init

--- settings.sh
+++ settings.sh
@@ -25,16 +25,16 @@ export NNS_DAPP_RELEASE="nightly-2023-10-30"
 # you can find NNS proposals upgrading system canisters here:
 # https://dashboard.internetcomputer.org/governance?topic=TOPIC_NETWORK_CANISTER_MANAGEMENT
 # NNS proposals to upgrade NNS frontend dapp are called "Upgrade Nns Canister: 3r4gx-wqaaa-aaaaq-aaaia-cai"
-export SNS_AGGREGATOR_RELEASE="proposal-129614-agg"
+export SNS_AGGREGATOR_RELEASE="nightly-2024-07-02"
 
 # only edit IC_COMMIT to a commit to master with disk image obtained via:
 # $ ./gitlab-ci/src/artifacts/newest_sha_with_disk_image.sh origin/master
 # from the IC monorepo: https://github.com/dfinity/ic
 # if you change IC_COMMIT, then you need to rerun `source install.sh`
-export IC_COMMIT="f79476803e097d9fd5f7e67d45f6818348b51ac9"
+export IC_COMMIT="2e269c77aa2f6b2353ddad6a4ac3d5ddcac196b1"
 
 export TESTNET="local"
 
-export DFX_VERSION="0.19.0"
+export DFX_VERSION="0.16.1"
 export DFX_SNS_VERSION="0.4.1"
 export DFX_NNS_VERSION="0.4.1"
